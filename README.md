7a82fd532ddb0897ee52952a4b2fb73004a57831
chamberezigbo <chamberezigbo@gmail.com>
2025-02-04 08:30:10 +0100

    added second task and route

--- README.md
+++ README.md
@@ -32,6 +32,40 @@ GET /hire/nodejs-developers
 }
 ```
 
+### 2. Classify a Number
+
+**Endpoint:**
+
+```
+GET /api/classify-number?number=<number>
+```
+
+**Request:**
+
+- Query parameter: `number` (integer)
+
+**Response (200 OK):**
+
+```json
+{
+  "number": 371,
+  "is_prime": false,
+  "is_perfect": false,
+  "properties": ["armstrong", "odd"],
+  "digit_sum": 11,
+  "fun_fact": "371 is an Armstrong number because 3^3 + 7^3 + 1^3 = 371"
+}
+```
+
+**Response (400 Bad Request):**
+
+```json
+{
+  "number": "alphabet",
+  "error": true
+}
+```
+
 ## Installation and Setup
 
 1. Clone the repository:
--- app.js
+++ app.js
@@ -1,10 +1,37 @@
 const cors = require("cors");
 const express = require("express");
+const axios = require("axios");
 
 const app = express();
 
 app.use(cors());
 
+// function to get prime numbers //
+function isPrime(num) {
+  if (num < 2) return false;
+  for (let i = 2; i <= Math.sqrt(num); i++) {
+    if (num % i === 0) return false;
+  }
+  return true;
+}
+
+function isPerfect(num) {
+  let sum = 1;
+  for (let i = 2; i <= num / 2; i++) {
+    if (num % i === 0) sum += i;
+  }
+  return sum === num && num !== 1;
+}
+
+// get is Armstrong number //
+function isArmstrong(num) {
+  const digits = num.toString().split("").map(Number);
+  const power = digits.length;
+  const sum = digits.reduce((acc, digit) => acc + Math.pow(digit, power), 0);
+  return sum === num;
+}
+
+// Stage One Task //
 app.get("/hire/nodejs-developers", (req, res) => {
   const current_datetime = new Date().toISOString(); //to get current datetime
 
@@ -13,6 +40,45 @@ app.get("/hire/nodejs-developers", (req, res) => {
     current_datetime,
     github_url: "https://github.com/Chamberezigbo/hng12.0",
   });
+});
+
+// Stage Two Task //
+app.get("/api/classify-number", async (req, res) => {
+  const { number } = req.query;
+
+  if (!number || isNaN(number)) {
+    return res.status(400).json({ number, error: true });
+  }
+
+  const num = parseInt(number);
+  const properties = [];
+  if (num % 2 !== 0) properties.push("odd");
+  if (num % 2 === 0) properties.push("even");
+  if (isPrime(num)) properties.push("prime");
+  if (isPerfect(num)) properties.push("perfect");
+  if (isArmstrong(num)) properties.push("armstrong");
+
+  const digitSum = num
+    .toString()
+    .split("")
+    .map(Number)
+    .reduce((a, b) => a + b, 0);
+
+  try {
+    const funFactResponse = await axios.get(`http://numbersapi.com/${num}`);
+    const funFact = funFactResponse.data;
+
+    return res.json({
+      number: num,
+      is_prime: isPrime(num),
+      is_perfect: isPerfect(num),
+      properties,
+      digit_sum: digitSum,
+      fun_fact: funFact,
+    });
+  } catch (error) {
+    return res.status(500).json({ error: "Error fetching fun fact" });
+  }
 });
 
 app.listen(3000, () => {
